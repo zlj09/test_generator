6876bb21b5454a0c2f725eb0a42fff8fc07bd18c
Zhu <lzhu308@gatech.edu>
2018-09-10 16:34:30 -0400

    Trying to override getValue

--- src/logic_simulator.py
+++ src/logic_simulator.py
@@ -1,41 +1,48 @@
+import re
+
 truth_table_dict = {
     "AND" : {(0, 0) : 0, (0, 1): 0, (1, 0): 0, (1, 1): 1},
     "OR" : {(0, 0) : 0, (0, 1): 1, (1, 0): 1, (1, 1): 1}
 }
 
 class Node:
-    driven = []
-    driving = []
-
     def addDriven(self, node):
         self.driven.append(node)
 
     def addDriving(self, node):
         self.driving.append(node)
+
+    def __init__(self):
+        self.driven = []
+        self.driving = []
+
+    def __str__(self):
+        return("Driven by " + str(len(self.driven)) + " nodes, Driving " + str(len(self.driving)) + " nodes")
     
 class Gate(Node):
-    logic = ""
-    truth_table = {}
-
     def __init__(self, gate_logic):
+        super().__init__()
         self.logic = gate_logic
         self.truth_table = truth_table_dict[gate_logic]
-        #self.driven = driven
-        #self.driving = driving
+
+
+    def __str__(self):
+        return("Gate " + self.logic + ", " + super(Gate, self).__str__())
+
+    def getValue(self):
+        value_list = []
+        unknown_list []
         
 class Wire(Node):
-    index = ""
-    value = -1
-
     def __init__(self, index):
+        super().__init__()
         self.index = index
+        self.value = -1
+    
+    def __str__(self):
+        return("Node " + self.index + ", " + super(Wire, self).__str__())
 
 class Circuit:
-    wire_dict = {}
-    gate_dict = {}
-    input_list = []
-    output_list = []
-
     def getWire(self, index):
         if index in self.wire_dict:
             return(self.wire_dict[index])
@@ -45,37 +52,74 @@ class Circuit:
             return(wire)
 
     def __init__(self, cir_name):
+        self.wire_dict = {}
+        self.gate_dict = {}
+        self.input_list = []
+        self.output_list = []
+
         fp = open("C:/Users/lzhu308/OneDrive - Georgia Institute of Technology\Academic\Digital Systems Testing/Projects/Logic simulator/logic_simulator/inputs/and_or.txt", "r")
         cir_lines = fp.readlines()
 
         for line in cir_lines:
-            words = line.split(" ")
+            if line[-1] == '\n':
+                line = line[:-1]
+            words = re.split(' ',line)
+
             if words[0] == "INPUT":
                 for index in words[1 : -1]:
-                    self.input_list.append(self.wire_dict[index])
+                    self.input_list.append(self.getWire(index))
             elif words[0] == "OUTPUT":
                for index in words[1 : -1]:
-                    self.output_list.append(self.wire_dict[index])
+                    self.output_list.append(self.getWire(index))
             else:
+                if (words[-1] == "-1"):
+                    words = words[:-1]
                 gate = Gate(words[0])
-                for wire_index in words[1 : -1]:
-                    if wire_index in self.wire_dict:
-                        wire = self.wire_dict[wire_index]
-                        gate.addDriven(wire)
-                        wire.addDriving(gate)
-                    else:
-                        wire = Wire(wire_index)
-                        self.wire_dict[wire_index] = wire
-                        gate.addDriven(wire)
-                        wire.addDriving(gate)
-                wire_index = words[-1]
+                for index in words[1 : -1]:
+                    wire = self.getWire(index)
+                    gate.addDriven(wire)
+                    wire.addDriving(gate)
+                index = words[-1]
+                wire = self.getWire(index)
+                gate.addDriving(wire)
+                wire.addDriven(gate)
 
+    def __str__(self):
+        cir_str = "Input: \n"
+        for wire in self.input_list:
+            cir_str = cir_str + str(wire) + "\n"
+        cir_str = cir_str + "Output: \n"
+        for wire in self.output_list:
+            cir_str = cir_str + str(wire) + "\n"
+        return(cir_str)
 
+    def initWire(self, inputs):
+        for wire in self.wire_dict.values():
+            wire.value = -1
+        for i in len(inputs):
+            self.input_list[i].value = inputs[i]
+
+    def getOutputs(self, inputs):
+        self.initWire(inputs)
+        wire_stack = []
+        for output_wire in self.output_list:
+            wire_stack.append(output_wire)
+            while (wire_stack):
+                wire = wire_stack.pop()
+                if (wire.value == -1):
+                    gate = wire.driven[0]
+                    unknown_cnt = 0
+                    tmp_stack = []
+                    for driving_wire in gate.driving:
+                        if (driving_wire.vaule == -1):
+                            unknown_cnt = unknown_cnt + 1
+                            tmp_stack.append(driving_wire)
             
                 
 if __name__ == "__main__":
     cir1 = Circuit("and_or")
-    print(cir1.wire_dict["1"].driving[0].driven[0].index)
+    print(cir1.wire_dict)
+    print(cir1)
 
 
     
